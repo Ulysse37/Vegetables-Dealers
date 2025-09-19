47a713acb59472b20ef1c77edaa238084bf7a5ff
Ulysse37 <ulysse.courtet@hotmail.fr>
2025-09-19 10:02:00 +0200

    ajout image home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import './home.css'
+import vegeEtal from '../../assets/vegeEtal.jpg'
 
 function Home() {
 
   return (
     <>
       <h1>Hello World</h1>
+      <figure>
+        <img src={vegeEtal} alt="Etal de légume" />
+      </figure>
     </>
   )
 }
