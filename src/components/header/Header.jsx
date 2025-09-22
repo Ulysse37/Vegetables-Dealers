00621b93dce928ea782e8602735a24148cfc5164
Ulysse37 <ulysse.courtet@hotmail.fr>
2025-09-23 00:11:12 +0200

    ajout logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import './header.css';
+import Logo from '../../assets/logo.svg';
 
 function Header() {
 
   return (
     <header className="header">
+      <img src={Logo} alt="logo" />
       <h1>La ferme Bouton </h1>
     </header>
   )
