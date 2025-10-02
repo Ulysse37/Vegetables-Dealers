3ea6471b556257f2a544a1a0426e0f497f00cc51
Ulysse37 <ulysse.courtet@hotmail.fr>
2025-10-02 09:33:34 +0200

    ajouts images gallery/carrousel

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,11 @@
 import './about.css';
 import OldCouple from '../../assets/aboutPage/oldFarmerCouple.jpg';
+/* import Farm1 from '../../assets/aboutPage/farm1.jpg';
+import Farm2 from '../../assets/aboutPage/farm2.jpg';
+import Farm3 from '../../assets/aboutPage/farm3.jpg';
+import Farm4 from '../../assets/aboutPage/farm4.jpg';
+import Farm5 from '../../assets/aboutPage/farm5.jpg';
+import Farm6 from '../../assets/aboutPage/farm6.jpg'; */
 
 function About() {
 
@@ -12,12 +18,14 @@ function About() {
           <figure>
             <img src={OldCouple} alt="Arrière grands-parents sur l'exploitation" />
           </figure>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
             commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
             fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-            mollit anim id est laborum.</p>
+            mollit anim id est laborum.
+          </p>
         </section>
       </main>
     </>
